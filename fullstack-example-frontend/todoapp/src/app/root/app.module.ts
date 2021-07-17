065e91a57a2a8111ba2c52ef7c431a1fbbc39b6d
Ramil Ilaev <ramil.ilaev@outlook.com>
2021-07-17 12:52:25 +0200

    Directory restructuring. Root module gets it's own directory since it will hold a few components like the navbar which all lazy loaded modules iwll use.

--- fullstack-example-frontend/todoapp/src/app/root/app.module.ts
+++ fullstack-example-frontend/todoapp/src/app/root/app.module.ts
@@ -1,8 +1,9 @@
+import { AppComponent } from './components/app/app.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
